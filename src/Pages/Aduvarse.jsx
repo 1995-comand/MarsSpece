7ce188fee7785681e0a120d27af39fe643586a4f
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2026-01-22 14:41:47 +0500

    asd

---
+++ src/Pages/Aduvarse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aduvarse = () => {
+  return (
+    <div>Aduvarse</div>
+  )
+}
+
+export default Aduvarse
---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Course from "./Pages/Course.jsx";
+import Aduvarse from "./Pages/Aduvarse.jsx";
+import Shop from "./Pages/Shop.jsx";
+import Code from "./Pages/Code.jsx";
+import Blog from "./Pages/Blog.jsx";
+import Profile from "./Pages/Profile.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/Course",
+        element: <Course />,
+      },
+      {
+        path: "/Aduverse",
+        element: <Aduvarse />,
+      },
+      {
+        path: "/Shop",
+        element: <Shop />,
+      },
+      {
+        path: "/Code",
+        element: <Code />,
+      },
+      {
+        path: "/Blog",
+        element: <Blog />,
+      },
+      {
+        path: "/Profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
