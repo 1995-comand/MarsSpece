a9712af0122bc078527b064299ed745ccf21a895
bekboss-YT <toraboyevbegzod11@gmail.com>
2026-01-22 15:00:57 +0500

    asd

--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Course = () => {
   return (
     <div>
-        
+        <div><h1>
+          </h1>
+          <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
     </div>
   )
 }
