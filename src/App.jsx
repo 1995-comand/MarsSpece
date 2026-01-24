d4c3e4233b3ce9c31c3a8b2985abd8d9bd6bdd7a
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2026-01-24 15:01:35 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 import { AppContext } from "./Context/AppContext";
 import logo from "./assets/mars logo.webp";
 import { ToastContainer } from "react-toastify";
+import Header from "./Components/Header";
 
 const App = () => {
   const [coin, SetCoin] = useState(0);
@@ -12,51 +13,10 @@ const App = () => {
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
-          <nav className="navbar w-full bg-base-300">
-            <label
-              htmlFor="my-drawer-4"
-              aria-label="open sidebar"
-              className="btn btn-square btn-ghost"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                strokeLinejoin="rond"
-                strokeLinecap="round"
-                strokeWidth="2"
-                fill="none"
-                stroke="currentColor"
-                className="my-1.5 inline-block size-4"
-              >
-                <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
-                <path d="M9 4v16"></path>
-                <path d="M14 10l2 2l-2 2"></path>
-              </svg>
-            </label>
-            <div className="px-4 flex items-center justify-between mx-auto w-[70%]">
-              <img className="w-[160px]" src={logo} alt="" />
-
-              <div className="flex items-center gap-6">
-                <p className="border-[1px] rounded border-[#adadad] w-fit px-2 py-1">
-                  🔥{coin}
-                </p>
-
-                <p className="border-[1px] rounded border-[#adadad] w-fit px-2 py-1">
-                  🪙{coin}
-                </p>
-
-                <p className="border-[1px] rounded border-[#adadad] w-fit px-2 py-1">
-                  ⚡{coin}
-                </p>
-
-                <button className="bg-gradient-to-r from-[#ce42ff] via-[#27b0fc ] to-[#3bf1da] text-white px-4 py-2 rounded font-bold text-[18px] hover-3d">
-                  Подписаться
-                </button>
-              </div>
-            </div>
-          </nav>
+     
 
           <div className="p-4">
+            <Header />
             <Outlet />
             <ToastContainer />
           </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import logo from "../assets/mars logo.webp";
+import { AppContext } from "../Context/AppContext";
 
 const Header = () => {
-  return <div>Header</div>;
+   const {coin, SetCoin} = useContext(AppContext);
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <img className="w-50" src={logo} alt="" />
+        </div>
+        <div className="flex gap-2">
+          <div>
+            <div className="flex items-center gap-6">
+              <p className="border-[1px] rounded border-[#adadad] w-fit px-2 py-1">
+                🔥{coin}
+              </p>
+
+              <p className="border-[1px] rounded border-[#adadad] w-fit px-2 py-1">
+                🪙{coin}
+              </p>
+
+              <p className="border-[1px] rounded border-[#adadad] w-fit px-2 py-1">
+                ⚡{coin}
+              </p>
+
+              <button className="bg-gradient-to-r from-[#ce42ff] via-[#27b0fc ] to-[#3bf1da] text-white px-4 py-2 rounded font-bold text-[18px] hover-3d">
+                Подписаться
+              </button>
+            </div>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
