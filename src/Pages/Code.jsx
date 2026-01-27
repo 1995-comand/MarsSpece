0c5c78f05fc2f9cdaa0b4388bcafa130d9c147fe
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2026-01-27 14:06:53 +0500

    asd

--- src/Pages/Code.jsx
+++ src/Pages/Code.jsx
@@ -5,11 +5,14 @@ const Code = () => {
     <div>
       <div className="ml-30">
         <h1 className="text-xl ml-11">Top Kompaniyalar</h1>
-        <img className="w-255 ml-11" src="https://space.marsit.uz/img/editor-ads.08b4da43.webp" alt="" />
+        <img
+          className="w-255 ml-11"
+          src="https://space.marsit.uz/img/editor-ads.08b4da43.webp"
+          alt=""
+        />
 
         <div className="min-h-screen bg-gray-50 p-6 mr-50">
           <div className="mx-auto max-w-5xl grid grid-cols-1 sm:grid-cols-2 gap-6">
-          
             <div className="relative flex items-center justify-between rounded-xl bg-white p-5 shadow-sm border border-gray-100">
               <div className="absolute top-4 right-4">
                 <span className="inline-flex h-6 w-6 items-center justify-center rounded-md bg-yellow-100 text-yellow-500 text-sm">
@@ -30,15 +33,13 @@ const Code = () => {
                   <p className="text-sm text-gray-500">
                     10 задач | 0% выполнено
                   </p>
-                    <button className="text-blue-600 text-sm font-medium hover:underline  flex justify-start">
-                <a href="https://www.amazon.com/"> Войти →</a>
-              </button>
+                  <button className="text-blue-600 text-sm font-medium hover:underline  flex justify-start">
+                    <a href="https://www.amazon.com/"> Войти →</a>
+                  </button>
                 </div>
-                
               </div>
             </div>
 
-         
             <div className="relative flex items-center justify-between rounded-xl bg-white p-5 shadow-sm border border-gray-100">
               <div className="absolute top-4 right-4">
                 <span className="inline-flex h-6 w-6 items-center justify-center rounded-md bg-yellow-100 text-yellow-500 text-sm">
@@ -59,13 +60,11 @@ const Code = () => {
                   <p className="text-sm text-gray-500">
                     11 задач | 0% выполнено
                   </p>
-                   <button className="text-blue-600 text-sm font-medium hover:underline">
-                <a href="https://www.google.com/?hl=ru"> Войти →</a>
-              </button>
+                  <button className="text-blue-600 text-sm font-medium hover:underline">
+                    <a href="https://www.google.com/?hl=ru"> Войти →</a>
+                  </button>
                 </div>
               </div>
-
-             
             </div>
 
             <div className="relative flex items-center justify-between rounded-xl bg-white p-5 shadow-sm border border-gray-100">
@@ -88,16 +87,13 @@ const Code = () => {
                   <p className="text-sm text-gray-500">
                     10 задач | 0% выполнено
                   </p>
-                    <button className="text-blue-600 text-sm font-medium hover:underline">
-                <a href="https://www.netflix.com/uz-ru/"> Войти →</a>
-              </button>
+                  <button className="text-blue-600 text-sm font-medium hover:underline">
+                    <a href="https://www.netflix.com/uz-ru/"> Войти →</a>
+                  </button>
                 </div>
               </div>
-
-            
             </div>
 
-    
             <div className="relative flex items-center justify-between rounded-xl bg-white p-5 shadow-sm border border-gray-100">
               <div className="absolute top-4 right-4">
                 <span className="inline-flex h-6 w-6 items-center justify-center rounded-md bg-yellow-100 text-yellow-500 text-sm">
@@ -118,13 +114,14 @@ const Code = () => {
                   <p className="text-sm text-gray-500">
                     12 задач | 0% выполнено
                   </p>
-                    <button className="text-blue-600 text-sm font-medium hover:underline">
-                <a href="https://www.facebook.com/?locale=ru_RU"> Войти →</a>
-              </button>
+                  <button className="text-blue-600 text-sm font-medium hover:underline">
+                    <a href="https://www.facebook.com/?locale=ru_RU">
+                      {" "}
+                      Войти →
+                    </a>
+                  </button>
                 </div>
               </div>
-
-            
             </div>
           </div>
         </div>
